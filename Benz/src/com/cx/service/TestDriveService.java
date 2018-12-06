f7102355753caa4479f4a5d07bab8f76d93f404e
unknown <1029153958@qq.com    name=1029153958>
2018-12-06 10:14:02 +0800

    最新变动

---
+++ Benz/src/com/cx/service/TestDriveService.java
@@ -0,0 +1,10 @@
+package com.cx.service;
+
+import com.cx.entity.TestDrive;
+
+public interface TestDriveService {
+	
+	TestDrive selectByTel(String tel);
+	
+	void insertTestDrive(TestDrive testdrive);
+}
